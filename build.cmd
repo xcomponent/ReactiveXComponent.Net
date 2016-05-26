c010e7ce9a75c42410901f9d3b942e6c06a95de7
yahyanajar <yahya.najar@xcomponent.com>
2016-05-26 15:43:32 +0200

    Reactive xc (#4)
    fake build scripts

---
+++ build.cmd
@@ -0,0 +1,16 @@
+@echo off
+
+pushd %~dp0
+
+if exist packages (
+	rmdir /s /q packages
+)
+Tools\NuGet.exe update -self
+
+Tools\NuGet.exe install FAKE -ConfigFile Tools\Nuget.Config -ExcludeVersion -OutputDirectory packages -Version 4.10.3
+Tools\NuGet.exe install nunit.runners -ConfigFile Tools\Nuget.Config -OutputDirectory .\packages\FAKE -ExcludeVersion -Version 2.6.4
+
+set encoding=utf-8
+packages\FAKE\tools\FAKE.exe build.fsx All
+
+popd

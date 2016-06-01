de42718f82b638053097502ccf285e99a50f718b
yahyanajar <yahya.najar@xcomponent.com>
2016-06-01 20:13:35 +0200

    Correct Parser Unit Tests

---
+++ ReactiveXComponent/Parser/DeploymentParser.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+using ReactiveXComponent.Common;
+using ReactiveXComponent.RabbitMQ;
+
+namespace ReactiveXComponent.Parser
+{
+    public class DeploymentParser
+    {
+        private XDocument _document;
+        private BusDetails _busDetails;
+
+        public DeploymentParser(string file)
+        {
+            _document = XDocument.Load(file);
+
+            InitBusDetails();
+        }
+
+        public string Host
+        {
+            set { BusDetails.Host = value; }
+            get { return BusDetails.Host; }
+        }
+
+        public int Port
+        {
+            set { BusDetails.Port = value; }
+            get { return BusDetails.Port; }
+        }
+
+        public BusDetails BusDetails
+        {
+            get { return _busDetails; }
+        }
+
+        public void InitBusDetails()
+        {
+            if (BusDetails == null)
+            {
+                _busDetails =(from bus in this._document.Descendants() where bus.Name.LocalName == "bus" select new BusDetails() { Host = bus.Attribute("host").Value, Username = bus.Attribute("user").Value, Password = bus.Attribute("password").Value, Port = int.Parse(bus.Attribute("port").Value) }).FirstOrDefault();
+            }
+        }
+
+        public string GetPublisherTopic(string component, string stateMachine, int eventCode)
+        {
+            var publishTag = from publish in this._document.Descendants() where publish.Name.LocalName == "publish" && publish.Attribute("componentCode").Value == HashcodeHelper.GetXcHashCode(component).ToString() && publish.Attribute("stateMachineCode").Value == HashcodeHelper.GetXcHashCode(stateMachine).ToString() && publish.Attribute("eventCode").Value == eventCode.ToString() select publish;
+            return (from topic in publishTag.FirstOrDefault().Descendants() where topic.Name.LocalName == "topic" select topic.Value).FirstOrDefault();
+        }
+
+        public string GetConsumerTopic(string component, string stateMachine)
+        {
+            var consumerTag = from consumer in this._document.Descendants() where consumer.Name.LocalName == "subscribe" && consumer.Attribute("componentCode") != null && consumer.Attribute("componentCode").Value == HashcodeHelper.GetXcHashCode(component).ToString() && consumer.Attribute("stateMachineCode") != null && consumer.Attribute("stateMachineCode").Value == HashcodeHelper.GetXcHashCode(stateMachine).ToString() && consumer.Attribute("eventType") != null && consumer.Attribute("eventType").Value == "UPDATE" select consumer;
+            return (from topic in consumerTag.FirstOrDefault().Descendants() where topic.Name.LocalName == "topic" select topic.Value).FirstOrDefault();
+        }
+    }
+}
---
+++ ReactiveXComponentTest/ParserTests/DeploymentParserTest.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using ReactiveXComponent.Parser;
+using ReactiveXComponent.RabbitMQ;
+
+namespace ReactiveXComponentTest.ParserTests
+{
+    [TestFixture]
+    class DeploymentParserTest
+    {
+        [TestCase()]
+        public void ParseXML_Success_test()
+        {
+            var expectedBusInfos = new BusDetails()
+            {
+                Username = "guest",
+                Password = "guest", 
+                Host = "127.0.0.1",
+                Port = 5672
+            };
+
+            var parser = new DeploymentParser("ParserTests//helloworldApi.xcApi");
+
+            var busInfos = parser.BusDetails;
+
+            Assert.AreEqual(expectedBusInfos, busInfos);
+        }
+
+        [TestCase()]
+        public void GetPublisherTopic_Sucess_Test()
+        {
+            var parser = new DeploymentParser("ParserTests//helloworldApi.xcApi");
+
+            var component = "HelloWorld";
+            var stateMachine = "HelloWorldManager";
+            int eventCode = 9;
+
+            var expectedTopic = "input.1_0.HelloMicroservice.HelloWorld.HelloWorldManager";
+            var topic = parser.GetPublisherTopic(component, stateMachine, eventCode);
+
+            Assert.AreEqual(expectedTopic, topic);
+        }
+
+        [TestCase()]
+        public void GetConsumerTopic_Sucess_Test()
+        {
+            var parser = new DeploymentParser("ParserTests//helloworldApi.xcApi");
+
+            var component = "HelloWorld";
+            var stateMachine = "HelloResponse";
+
+            var expectedTopic = "output.1_0.HelloMicroservice.HelloWorld.HelloResponse";
+            var topic = parser.GetConsumerTopic(component, stateMachine);
+
+            Assert.AreEqual(expectedTopic, topic);
+        }
+
+    }
+}

4ecc2d70751028d86ff6fd089887481f24aefb6a
yahyanajar <yahya.najar@xcomponent.com>
2016-06-07 19:50:57 +0200

    Connection Integration Test disabled

--- ReactiveXComponentTest/RabbitMqTests/RabbitMqConnectionTest.cs
+++ ReactiveXComponentTest/RabbitMqTests/RabbitMqConnectionTest.cs
@@ -9,37 +9,37 @@ namespace ReactiveXComponentTest
     [Category("Integration Tests")]
     public class RabbitMqConnectionTest
     {
-        private BusDetails _busInfos;
-        [SetUp]
-        public void Setup()
-        {
-            _busInfos = new BusDetails
-            {
-                Host = "localhost",
-                Username = "",
-                Password = "",
-                Port = 5672
-            };
-        }
-        
-
-        [TestCase("guest","guest")]
-        public void CreateConnection_GivenBusDetails_ShouldOpenConnexion_test(string username, string password)
-        {
-            //Arrange
-            _busInfos.Username = username;
-            _busInfos.Password = password;
-            bool isOpen;
-            
-            //Act
-            using (var rabbitMqconnection = new RabbitMqConnection(_busInfos))
-            {
-                var connection = rabbitMqconnection.GetConnection();
-                isOpen = connection.IsOpen;
-            }
-
-            //Assert
-            Assert.IsTrue(isOpen);
-        }
+    //    private BusDetails _busInfos;
+    //    [SetUp]
+    //    public void Setup()
+    //    {
+    //        _busInfos = new BusDetails
+    //        {
+    //            Host = "localhost",
+    //            Username = "",
+    //            Password = "",
+    //            Port = 5672
+    //        };
+    //    }
+    //    
+    //
+    //    [TestCase("guest","guest")]
+    //    public void CreateConnection_GivenBusDetails_ShouldOpenConnexion_test(string username, string password)
+    //    {
+    //        //Arrange
+    //        _busInfos.Username = username;
+    //        _busInfos.Password = password;
+    //        bool isOpen;
+    //        
+    //        //Act
+    //        using (var rabbitMqconnection = new RabbitMqConnection(_busInfos))
+    //        {
+    //            var connection = rabbitMqconnection.GetConnection();
+    //            isOpen = connection.IsOpen;
+    //        }
+    //
+    //        //Assert
+    //        Assert.IsTrue(isOpen);
+    //    }
     }
 }

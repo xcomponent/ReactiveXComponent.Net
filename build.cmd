d116ece74181ddf1c4ab609371c95faa7c41a59c
yahyanajar <yahya.najar@xcomponent.com>
2016-06-09 16:31:27 +0200

    Minor Fix in BuildScript

--- build.cmd
+++ build.cmd
@@ -6,10 +6,8 @@ if exist packages (
 	rmdir /s /q packages
 )
 
-Tools\Nuget\NuGet.exe update -self
-
-Tools\Nuget\NuGet.exe install FAKE -ConfigFile Tools\Nuget\Nuget.Config -ExcludeVersion -OutputDirectory packages -Version 4.10.3
-Tools\Nuget\NuGet.exe install nunit.runners -ConfigFile Tools\Nuget\Nuget.Config -OutputDirectory .\packages\FAKE -ExcludeVersion -Version 2.6.4
+Tools\NuGet.exe install FAKE -ConfigFile Tools\Nuget.Config -ExcludeVersion -OutputDirectory packages -Version 4.10.3
+Tools\NuGet.exe install nunit.runners -ConfigFile Tools\Nuget.Config -OutputDirectory .\packages\FAKE -ExcludeVersion -Version 2.6.4
 
 set encoding=utf-8
 packages\FAKE\tools\FAKE.exe build.fsx All

1b9500de8914ed0554f71312dbce2d9d4fbcbd7e
yahya.najar <yahya.najar@xcomponent.com>
2016-07-22 16:41:20 +0200

    Creation of ClientAPIGenerator

---
+++ XComponentClientApi/Settings.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace XComponentClientApi
+{
+    internal class Settings
+    {
+        public string CSFileName { get; set; }
+        public string XmlFileName { get; set; }
+
+        public bool IsValid()
+        {
+            if (string.IsNullOrEmpty(CSFileName) || !File.Exists(CSFileName)) return false;
+            return !string.IsNullOrEmpty(XmlFileName) && File.Exists(XmlFileName);
+        }
+    }
+}
---
+++ XComponentClientApi/XCClientApi.cs
@@ -0,0 +1,13 @@
+﻿
+namespace XComponentClientApi
+{
+    public partial class XCClientApi
+    {
+        private readonly string _xmlFile;
+    
+        public XCClientApi(string xmlFile)
+        {
+            _xmlFile = xmlFile;
+        }
+    }
+}

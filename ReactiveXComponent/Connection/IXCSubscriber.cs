38e339715aac2a1de0140decadf1c9b3f11ba785
yahya.najar <yahya.najar@xcomponent.com>
2016-10-31 14:34:25 +0100

    Fix in RabbitMqSubscriber

--- ReactiveXComponent/Connection/IXCSubscriber.cs
+++ ReactiveXComponent/Connection/IXCSubscriber.cs
@@ -7,7 +7,7 @@ namespace ReactiveXComponent.Connection
     public interface IXCSubscriber : IDisposable
     {
         void Subscribe(string stateMachine, Action<MessageEventArgs> stateMachineListener);
-        void Unsubscribe(string stateMachine, Action<MessageEventArgs> stateMachineListener);
+        void Unsubscribe(string stateMachine);
         IObservable<MessageEventArgs> StateMachineUpdatesStream { get; }
     }
 }

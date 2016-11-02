556e2b1b2ec930b9b5f5b8239907b9140e58513d
yahya.najar <yahya.najar@xcomponent.com>
2016-11-02 18:38:33 +0100

    RabbitMqSubscriber : modif in Unsubscribe method

--- ReactiveXComponentTest/IntegrationTests/RabbitMqCommunicationTest.cs
+++ ReactiveXComponentTest/IntegrationTests/RabbitMqCommunicationTest.cs
@@ -183,7 +183,7 @@ namespace ReactiveXComponentTest.IntegrationTests
 
             var newMessageReceived = lockEvent.WaitOne(timeoutReceive);
 
-            subscriber?.Unsubscribe(StateMachineA);
+            subscriber?.Unsubscribe(StateMachineA, messagedReceivedHandler);
 
             Check.That(newMessageReceived).IsTrue();
 
@@ -261,7 +261,7 @@ namespace ReactiveXComponentTest.IntegrationTests
                
                 var messageReceived = lockEvent.WaitOne(timeoutReceive);
 
-                subscriber.Unsubscribe(StateMachineA);
+                subscriber.Unsubscribe(StateMachineA, messagedReceivedHandler);
 
                 Check.That(messageReceived).IsTrue();
                 Check.That(label).IsNotNull().And.IsNotEmpty().And.Equals(TestMessage);

e4f71fa6fc55594f79c14148a9f1a3139753392b
anouarhassine <anouar.hassine@xcomponent.com>
2017-01-10 16:01:23 +0100

    RabbitMq - Fixing private subscription (#23)

--- ReactiveXComponent/RabbitMq/RabbitMqSubscriberInfos.cs
+++ ReactiveXComponent/RabbitMq/RabbitMqSubscriberInfos.cs
@@ -1,11 +1,40 @@
-﻿using RabbitMQ.Client;
+﻿using System;
+using System.Collections.Generic;
+using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
 namespace ReactiveXComponent.RabbitMq
 {
     public class RabbitMqSubscriberInfos
     {
+        public RabbitMqSubscriberInfos()
+        {
+            Handlers = new List<EventHandler<BasicDeliverEventArgs>>();
+        }
+
+        public RabbitMqSubscriberInfos(IModel channel, EventingBasicConsumer subscriber)
+        {
+            Channel = channel;
+            Subscriber = subscriber;
+            Handlers = new List<EventHandler<BasicDeliverEventArgs>>();
+        }
+
+        public RabbitMqSubscriberInfos(IModel channel, EventingBasicConsumer subscriber, EventHandler<BasicDeliverEventArgs> handler)
+        {
+            Channel = channel;
+            Subscriber = subscriber;
+            Subscriber.Received += handler;
+            Handlers = new List<EventHandler<BasicDeliverEventArgs>>() { handler };
+        }
+
+        public void AddHandler(EventHandler<BasicDeliverEventArgs> handler)
+        {
+            Subscriber.Received += handler;
+            Handlers.Add(handler);
+        }
+
         public IModel Channel { get; set; }
         public EventingBasicConsumer Subscriber { get; set; }
+        public List<EventHandler<BasicDeliverEventArgs>> Handlers { get; private set; } 
     }
 }

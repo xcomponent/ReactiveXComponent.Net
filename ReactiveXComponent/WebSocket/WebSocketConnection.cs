892d76a037cc6744c9ac170b70ef56d19d7d507f
anouarhassine <anouar.hassine@xcomponent.com>
2017-01-13 11:46:17 +0100

    Refactoring IXCSession for WebSocket implementation and updating the documentation accordingly

--- ReactiveXComponent/WebSocket/WebSocketConnection.cs
+++ ReactiveXComponent/WebSocket/WebSocketConnection.cs
@@ -1,69 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading;
-using System.Threading.Tasks;
 using ReactiveXComponent.Common;
 using ReactiveXComponent.Configuration;
 using ReactiveXComponent.Connection;
-using WebSocketSharp;
 
 namespace ReactiveXComponent.WebSocket
 {
     public class WebSocketConnection : IXCConnection
     {
-        private readonly IWebSocketClient _webSocketClient;
+        private readonly WebSocketEndpoint _endpoint;
+        private readonly int _timeout;
         private readonly IXCConfiguration _xcConfiguration;
         private readonly string _privateCommunicationIdentifier;
 
         public WebSocketConnection(IXCConfiguration xcConfiguration, WebSocketEndpoint endpoint, int timeout, string privateCommunicationIdentifier = null)
         {
-            _webSocketClient = new WebSocketClient();
-            _webSocketClient.Init(endpoint, timeout);
-            _webSocketClient.Open();
+            _endpoint = endpoint;
+            _timeout = timeout;
             _xcConfiguration = xcConfiguration;
             _privateCommunicationIdentifier = privateCommunicationIdentifier;
         }
 
         public IXCSession CreateSession()
         {
-            return new WebSocketSession(_webSocketClient, _xcConfiguration, _privateCommunicationIdentifier);
+            return new WebSocketSession(_endpoint, _timeout, _xcConfiguration, _privateCommunicationIdentifier);
         }
-
-
-        #region IDisposable implementation
-
-        private bool _disposed;
-
-        private void Dispose(bool disposing)
-        {
-            if (!_disposed)
-            {
-                if (disposing)
-                {
-                    _webSocketClient.Dispose();
-                }
-
-                // clear unmanaged resources
-
-                _disposed = true;
-            }
-        }
-
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        ~WebSocketConnection()
-        {
-            Dispose(false);
-        }
-
-        #endregion
-
     }
 }
--- ReactiveXComponent/WebSocket/WebSocketSession.cs
+++ ReactiveXComponent/WebSocket/WebSocketSession.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading;
-using System.Threading.Tasks;
 using ReactiveXComponent.Common;
 using ReactiveXComponent.Configuration;
 using ReactiveXComponent.Connection;
@@ -17,9 +11,11 @@ namespace ReactiveXComponent.WebSocket
         private readonly IXCConfiguration _xcConfiguration;
         private readonly string _privateCommunicationIdentifier;
 
-        public WebSocketSession(IWebSocketClient webSocketClient, IXCConfiguration xcConfiguration, string privateCommunicationIdentifier)
+        public WebSocketSession(WebSocketEndpoint endpoint, int timeout, IXCConfiguration xcConfiguration, string privateCommunicationIdentifier)
         {
-            _webSocketClient = webSocketClient;
+            _webSocketClient = new WebSocketClient();
+            _webSocketClient.Init(endpoint, timeout);
+            _webSocketClient.Open();
             _xcConfiguration = xcConfiguration;
             _privateCommunicationIdentifier = privateCommunicationIdentifier;
         }
@@ -33,5 +29,37 @@ namespace ReactiveXComponent.WebSocket
         {
             return new WebSocketSubscriber(component, _webSocketClient, _xcConfiguration, _privateCommunicationIdentifier);
         }
+
+        #region IDisposable implementation
+
+        private bool _disposed;
+
+        private void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _webSocketClient.Dispose();
+                }
+
+                // clear unmanaged resources
+
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~WebSocketSession()
+        {
+            Dispose(false);
+        }
+
+        #endregion
     }
 }

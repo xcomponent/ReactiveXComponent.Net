d3ec514b6696d1553b566d09edb1afc553ee6f42
Anouar Hassine <anouar.hassine@xcomponent.com>
2017-05-31 10:41:10 +0200

    Adding received message casting helper (#48)

---
+++ ReactiveXComponentTest/Common/MessageEventArgsCastTests.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using NFluent;
+using NUnit.Framework;
+using ReactiveXComponent.Common;
+using ReactiveXComponent.Serializer;
+
+namespace ReactiveXComponentTest.Common
+{
+    [TestFixture]
+    public class MessageEventArgsCastTests
+    {
+        [TestCase(SerializationType.Binary)]
+        [TestCase(SerializationType.Json)]
+        [TestCase(SerializationType.Bson)]
+        public void GetMessageTest(SerializationType serializationType)
+        {
+            var testObject = new TestObject()
+            {
+                Id = 101,
+                Name = "Test"
+            };
+
+            var serializer = SerializerFactory.CreateSerializer(serializationType);
+            using (var stream = new MemoryStream())
+            {
+                serializer.Serialize(stream, testObject);
+                stream.Position = 0;
+
+                var deserializedObject = serializer.Deserialize(stream);
+                var messageEventArgs = new MessageEventArgs(null, deserializedObject);
+                var resultObject = messageEventArgs.GetMessage<TestObject>(serializationType);
+                Check.That(resultObject).IsNotNull();
+                Check.That(resultObject).IsEqualTo(testObject);
+            }
+        }
+    }
+}
---
+++ ReactiveXComponentTest/Common/TestObject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ReactiveXComponentTest.Common
+{
+    [Serializable]
+    public class TestObject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        protected bool Equals(TestObject other)
+        {
+            return Id == other.Id && string.Equals(Name, other.Name);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((TestObject)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Id * 397) ^ (Name != null ? Name.GetHashCode() : 0);
+            }
+        }
+    }
+}

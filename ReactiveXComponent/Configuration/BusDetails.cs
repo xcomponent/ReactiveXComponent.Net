2356821d0e1862d237fd2e9058cf3c175603c2eb
Anouar Hassine <anouar.hassine@xcomponent.com>
2017-12-08 14:47:43 +0100

    Adding possibility to override communication configuration (#60) (#61)

--- ReactiveXComponent/Configuration/BusDetails.cs
+++ ReactiveXComponent/Configuration/BusDetails.cs
@@ -1,7 +1,13 @@
+
 namespace ReactiveXComponent.Configuration
 {
     public class BusDetails
     {
+        public BusDetails()
+        {
+            
+        }
+
         public BusDetails(string username, string password, string host, int port)
         {
             Username = username;
@@ -10,36 +16,21 @@ namespace ReactiveXComponent.Configuration
             Port = port;
         }
 
-        public string Username { get; }
+        public string Username { get; set; }
 
-        public string Password { get; }
+        public string Password { get; set; }
 
-        public string Host { get; }
+        public string Host { get; set; }
 
-        public int Port { get; }
+        public int Port { get; set; }
 
-        public override bool Equals(object obj)
+        public BusDetails Clone()
         {
-            var toCompareWith = obj as BusDetails;
-        
-            return toCompareWith != null && Equals(toCompareWith);
-        }
-
-        private bool Equals(BusDetails other)
-        {
-            return string.Equals(Username, other.Username) && string.Equals(Password, other.Password) && string.Equals(Host, other.Host) && Port == other.Port;
-        }
-        
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                var hashCode = Username?.GetHashCode() ?? 0;
-                hashCode = (hashCode*397) ^ (Password?.GetHashCode() ?? 0);
-                hashCode = (hashCode*397) ^ (Host?.GetHashCode() ?? 0);
-                hashCode = (hashCode*397) ^ Port;
-                return hashCode;
-            }
+            return new BusDetails(
+                (string)Username?.Clone(),
+                (string)Password?.Clone(),
+                (string)Host?.Clone(),
+                Port);
         }
     }
 }

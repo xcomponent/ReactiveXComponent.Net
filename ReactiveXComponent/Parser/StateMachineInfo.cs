24614f77c04075817dca8ef92e05387f09cf41df
yahyanajar <yahya.najar@xcomponent.com>
2018-02-01 16:39:01 +0100

    Fix in XCAPI parser (porting from master branch)
    Fix in XCAPI parser for the case where different components have the same stateMachine name

---
+++ ReactiveXComponent/Parser/StateMachineInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace ReactiveXComponent.Parser
+{
+    public class StateMachineInfo
+    {
+        public string StateMachineName { get; }
+        public int StateMachineCode { get; }
+
+        public StateMachineInfo(string stateMachineName, int stateMachineCode)
+        {
+            StateMachineName = stateMachineName;
+            StateMachineCode = stateMachineCode;
+        }
+    }
+}

433e8eda8a7736033c7b23bc865c0ae5b038265e
fle-bach <florian.de-la-riviere@xcomponent.com>
2019-02-14 15:03:08 +0100

    delete folder Scripts generate by Xcomponent and add in .gitignore

--- docker/integration_tests/XCProjects/HelloWorldV5/Scripts/generatePackage.bat
+++
@@ -1,7 +0,0 @@
-@rem GENERATE PACKAGE BATCH FILE
-if [%1]==[] (
-	mkdir "../generatedPackage"
-	XComponent.XCTools.exe --compilationmode=Debug --generatePackage -env=Dev -output="../generatedPackage" -project="../HelloWorldV5_Model.xcml"
-) else (
-	XComponent.XCTools.exe --compilationmode=Debug --generatePackage -env=Dev -output=%1 -project="../HelloWorldV5_Model.xcml"
-)
